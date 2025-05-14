08f584f3d55ff67eb61ff5e24af5acba1670d221
SarathyS101 <se.sarathy17@gmail.com>
2025-05-14 15:21:34 -0400

    added background video

--- tangent/app/page.tsx
+++ tangent/app/page.tsx
@@ -9,19 +9,19 @@
 //     </div>
 //   );
 // }
-import { GalleryVerticalEnd } from "lucide-react"
-import { LoginForm } from "@/components/ui/login-form"
+import { GalleryVerticalEnd } from "lucide-react";
+import { LoginForm } from "@/components/ui/login-form";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="/view-tangents" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            {<i>tan (θ)</i>} 
+          <a
+            href="/view-tangents"
+            className="flex items-center gap-2 font-medium"
+          >
+            {<i>tan (θ) by Sarathy Selvam</i>}
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
@@ -30,13 +30,18 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+      <div className="relative hidden lg:block p-2">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover border-8 border-white rounded-lg shadow-lg"
+        >
+          <source src="/background.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
     </div>
-  )
+  );
 }
